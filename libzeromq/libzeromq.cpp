480bdbe5686ed9d7ea55e1577185192cba485567
weiwei <wxjlmr@126.com>
2020-06-27 18:18:31 +0800

    创建动态库框架

---
+++ libzeromq/libzeromq.cpp
@@ -0,0 +1,5 @@
+#include "libzeromq.h"
+
+Libzeromq::Libzeromq()
+{
+}
---
+++ libzeromq/libzeromq.h
@@ -0,0 +1,12 @@
+#ifndef LIBZEROMQ_H
+#define LIBZEROMQ_H
+
+#include "libzeromq_global.h"
+
+class LIBZEROMQ_EXPORT Libzeromq
+{
+public:
+    Libzeromq();
+};
+
+#endif // LIBZEROMQ_H
